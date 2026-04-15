b98084567a768680be4234dd18e5eb2650636235
prathammorye77 <prathammorye77@gmail.com>
2026-04-15 13:36:16 +0530

    updated error favourite notification

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -1,19 +1,15 @@
 import axios from "../utils/axiosInstance";
 import { useState } from "react";
 import { toast } from "react-toastify";
-import { useAuth } from "../context/AuthContext";
 
 function FavoriteButton({ listingId, isFavInitial }) {
   const [isFav, setIsFav] = useState(isFavInitial);
-  const {userFav} = useAuth();
 
   const handleFavorite = async () => {
     try {
       const res = await axios.post(`/favorites/${listingId}`);
       setIsFav((setIsFav) => !setIsFav);
-      userFav(res.data.favorites);
-      
-      console.log(res.data.favorites)
+      console.log(res.data.favorites);
       toast.success(res.data.message);
     } catch (err) {
       toast.error("Error updating favorite");
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -8,7 +8,6 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
 
- 
   const login = (userData, token) => {
     localStorage.setItem("token", token);
     localStorage.setItem("user", JSON.stringify(userData));
