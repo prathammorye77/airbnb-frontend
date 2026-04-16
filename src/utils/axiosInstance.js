faa4e71bede560fcd4dbc3d36557e3d45b95fec8
prathammorye77 <prathammorye77@gmail.com>
2026-04-16 14:32:17 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
-  baseURL:'API',
+  baseURL:API,
 });
 
 axiosInstance.interceptors.request.use((config) => {
